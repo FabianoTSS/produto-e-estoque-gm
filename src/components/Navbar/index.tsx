828acfc550a70cfe28ebbf2e6a6046402345491e
Fabiano Gabriel Tassotti <fabianotassotti@hotmail.com>
2021-07-25 13:52:31 -0300

    Melhorias de estilizaçao

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 //import Home from './pages/Home';
-
+import { Container }  from './style';
 import Logotipo from '../../imagens/logotipo.png';
 
 const Navbar: React.FC = () => {
   return(
+    <Container>
       <header>
         <img src={Logotipo} alt="FABIANUS LOGOTIPO"/>
         <nav>
-            <a href="www.google.com">Homepage</a>
+            <a href="/">Homepage</a>
             <a href="ProductRegister">Produtos</a>
             <a href="RegisterUser">Cadastre-se</a>
         </nav>
       </header>
+    </Container>
   );
 }
 
--- src/components/Navbar/style.ts
+++ src/components/Navbar/style.ts
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+header{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+header nav a {
+    margin: 0px 0px 0px 40px;
+}
 `
