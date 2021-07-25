966e3616c2a825179a4b19a9a226718e3bdb6ccb
Fabiano Gabriel Tassotti <fabianotassotti@hotmail.com>
2021-07-24 22:17:04 -0300

    Adicionado Navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import Home from './pages/Home';
+
+const Navbar: React.FC = () => {
+  return(
+      <>
+        Este é o NavBar
+      </>
+  );
+}
+
+export default Navbar;
